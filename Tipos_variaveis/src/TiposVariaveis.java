aa7374b2440d32370ba82ed96abf77e741610724
carlosolive <carlosolive7551@gmail.com>
2022-10-18 21:17:36 -0300

    iniciando tipos de variaveis

--- Tipos_variaveis/src/TiposVariaveis.java
+++ Tipos_variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //tipos inteiros
+        byte numeroPequeno = 1;
+        short numeroCurto = 2022;
+        int numeroNormal = 1009909;
+        long numeroGrande = 1983727637626L;
+
+        //decimais
+        float peso = 55.7F;
+        double salario = 129747788.98;
+
+        //boolean
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        //Caracter
+        char letra = 'A';
+        String nome = "Carlos"; //String é um objeto e não um tipo primitivo
+        
     }
 }
